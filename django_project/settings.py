ace12e261d6b3f9a977b0b35a95bba6e725ab498
Irasubiza Tharcisse <irasubizatharcisse23@tuta.io>
2026-02-14 16:18:26 +0200

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,11 +81,18 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
+# ...
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This looks for an environment variable named DATABASE_URL or POSTGRES_URL
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
