12ca8cd6323d8c3801694594913df6e08441e88e
Irasubiza Tharcisse <irasubizatharcisse23@tuta.io>
2026-02-14 16:06:28 +0200

    Use environment variables for SECRET_KEY and DEBUG
    Updated SECRET_KEY and DEBUG settings to use environment variables for better security.

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,10 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my-portofolio'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['.vercel.app']
 CSRF_TRUSTED_ORIGINS = [
